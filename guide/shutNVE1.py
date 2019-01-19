3b6a0629aafbe846fab26d5f3a20144430d9f603
simon <sihart@cisco.com>
2019-01-19 11:04:39 +0000

    ShutNVE, step3 and step7

--- guide/shutNVE1.py
+++ guide/shutNVE1.py
@@ -1,7 +1,7 @@
 from genie.conf import Genie
 from genie.conf.base import Interface as Intf_conf
 
-testbed = Genie.init('sandbox_iosxe.yaml')
+testbed = Genie.init('vagrant_single_ios.yaml')
 
 uut = testbed.devices.iosxe1
 uut.connect()
